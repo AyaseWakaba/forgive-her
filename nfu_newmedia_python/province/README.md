d70914b147554d91839bb8e663858727c4ca45db
AyaseWakba <275865226@qq.com>
2017-06-21 06:27:44 +0800

    ver2

---
+++ nfu_newmedia_python/province/README.md
@@ -0,0 +1,29 @@
+Province abbreviation and its Internet popularity rate 
+
+		
+# 简介 
+网上搜索省份简称及其该省的互联网普及率
+
+
+		
+
+## 输入：
+省份名字
+## 输出：
+省份的简称及其互联网普及率
+## 从输入到输出，本组作品使用了：
+### 模块
+*requests
+*opencv
+### 数据
+*[国家数据统计局官网](http://data.stats.gov.cn/easyquery.htm?cn=E0103)
+
+## 作者成员：
+Baizui
+wanlihon
+luochangming
+luo00789
+454770749
+Butterrrr
+
+		
---
+++ nfu_newmedia_python/province/province/province.py
@@ -0,0 +1,65 @@
+from flask import Flask, render_template, request, escape
+import json
+import pandas as pd
+#from vsearch import search4letters
+
+app = Flask(__name__)
+ 
+with open ('data/province_code_name.json') as fp:
+    data = json.load(fp)    
+    list_province = [v for k,v in data.items()]
+    
+
+def get_data():
+    df = pd.DataFrame.from_csv("fsnd_data.tsv", encoding='utf8', sep='\t')
+    int_pr = df.query("zb=='A0G0H05'")[['reg','sj', 'data']]
+    temp = df[['reg','sj','data']].set_index('reg').to_dict()['data']
+    return temp
+
+def log_request(req: 'flask_request', res: str) -> None:
+    """Log details of the web request and the results."""
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+
+
+@app.route('/search_province', methods=['POST'])
+def do_search() -> 'html':
+    province = request.form['province']
+    title = '以下是您的结果：'
+    results = get_data()
+    return render_template('results.html',
+                           the_title=title,
+                           the_province=province,
+						   the_results = results[110000]
+						   )
+                           #the_results=results,)
+
+
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    """Display this webapp's HTML form."""
+    return render_template('entry.html',
+                           the_title='欢迎来到网上省份与互联网普及率搜索!',
+						   the_user_province=list_province)
+    
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    """Display the contents of the log file as a HTML table."""
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')       
+    return render_template('viewlog.html',
+                           the_title='用户您获取了以下的数据：',
+                           the_row_titles=titles,
+                           the_data=contents,)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
