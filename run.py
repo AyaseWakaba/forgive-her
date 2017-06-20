c0adc8d20696b45fd1ce0bcf3b1d48c7d0bf4507
AyaseWakaba <275865226@qq.com>
2017-06-21 06:31:56 +0800

    wdqwqd
    dwq

--- run.py
+++ run.py
@@ -2,7 +2,11 @@
 # coding: utf-8
 
 # In[11]:
-
+app.run(debug = True)port app
+app import app
+app.run(debug = True)from app import app
+app.run(debug = True)from app import app
+app.run(debug = True)
 from app import app
 app.run(debug = True)
 
