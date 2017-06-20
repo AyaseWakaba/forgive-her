d7fb423f2b37036696ce118e9c1074e4f4a51cd1
AyaseWakaba <275865226@qq.com>
2017-06-21 06:37:15 +0800

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -4,6 +4,3 @@ from flask.ext.wtf import Required
 class LoginForm(Form):
     openid = TextField('openid', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
-sadsadsadsa
-sdsadsadsadsad
-dsad
